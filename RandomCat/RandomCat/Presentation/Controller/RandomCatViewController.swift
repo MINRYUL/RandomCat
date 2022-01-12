771f12cbbd88e45b60c5b53298c88dfa71fc0a60
MinChang <rud0806@gmail.com>
2022-01-12 17:29:58 +0900

    [Feat] 폴더링 및 TabBarController 생성

--- RandomCat/RandomCat/Presentation/Controller/RandomCatViewController.swift
+++ RandomCat/RandomCat/Presentation/Controller/RandomCatViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RandomCatViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ RandomCat/RandomCat/Presentation/Controller/TabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarController.swift
+//  RandomCat
+//
+//  Created by 김민창 on 2022/01/12.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
