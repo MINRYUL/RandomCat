4a6cd0cd296f265c288d160b6ee81f8b6044a021
MinChang <rud0806@gmail.com>
2022-01-12 18:39:18 +0900

    [Feat] Configure Diffable CollcetionView

---
+++ RandomCat/RandomCat/Presentation/Cell/RandomCatCollectionViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  RandomCatCollectionViewCell.swift
+//  RandomCat
+//
+//  Created by 김민창 on 2022/01/12.
+//
+
+import UIKit
+
+class RandomCatCollectionViewCell: UICollectionViewCell {
+    static let identifier = "RandomCatCell"
+    
+    lazy private var catImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "CatLogo")?.withAlignmentRectInsets(UIEdgeInsets(top: -50, left: -50, bottom: -50, right: -50))
+        imageView.tintColor = .label
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.configureView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureView() {
+        self.addSubview(self.catImage)
+        self.backgroundColor = .systemGray6
+        NSLayoutConstraint.activate([
+            self.catImage.topAnchor.constraint(equalTo: self.topAnchor),
+            self.catImage.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.catImage.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            self.catImage.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+}
--- RandomCat/RandomCat/Presentation/Controller/RandomCatViewController.swift
+++ RandomCat/RandomCat/Presentation/Controller/RandomCatViewController.swift
@@ -6,8 +6,16 @@
 //
 
 import UIKit
+import SwiftUI
 
 class RandomCatViewController: UIViewController {
+    enum RandomCatSection: Int, CaseIterable {
+        case main
+    }
+    
+    typealias RandomCatDataSource = UICollectionViewDiffableDataSource<RandomCatSection, AnyHashable>
+    typealias RandomCatSnapshot = NSDiffableDataSourceSnapshot<RandomCatSection, AnyHashable>
+    
     lazy private var randomCollectionView: UICollectionView = {
         let flowLayout = UICollectionViewFlowLayout()
         let collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: 0, height: 0), collectionViewLayout: flowLayout)
@@ -15,16 +23,36 @@ class RandomCatViewController: UIViewController {
         return collectionView
     }()
     
+    private var dataSource: RandomCatDataSource?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        
+        self.configureView()
+        self.configureCollectionView()
+        self.configureDataSource()
+        self.configureBinding()
+    }
+    
+    private func bindSnapShotApply(section: RandomCatSection, item: [AnyHashable]) {
+        DispatchQueue.main.async { [weak self] in
+            var snapshot = RandomCatSnapshot()
+            snapshot.appendSections([.main])
+            item.forEach {
+                snapshot.appendItems([$0], toSection: section)
+            }
+            self?.dataSource?.apply(snapshot, animatingDifferences: true)
+        }
+    }
+    
+    private func configureBinding() {
+        let data = [1, 2, 3, 4, 5, 6, 7]
+        self.bindSnapShotApply(section: .main, item: data)
     }
 
     private func configureView() {
         self.view.addSubview(self.randomCollectionView)
-        
+        self.view.backgroundColor = .systemBackground
         NSLayoutConstraint.activate([
             self.randomCollectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
             self.randomCollectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
@@ -32,6 +60,45 @@ class RandomCatViewController: UIViewController {
             self.randomCollectionView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
+    
+    private func configureCollectionView() {
+        self.randomCollectionView.delegate = self
+        self.randomCollectionView.collectionViewLayout = self.configureCompositionalLayout()
+        
+        self.randomCollectionView.register(RandomCatCollectionViewCell.self, forCellWithReuseIdentifier: RandomCatCollectionViewCell.identifier)
+    }
+    
+    private func configureCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { (_, _) -> NSCollectionLayoutSection? in
+            let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1.0)))
+            item.contentInsets = .init(top: 3, leading: 3, bottom: 3, trailing: 3)
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.5)), subitems: [item])
+            let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .none
+            section.contentInsets = .init(top: 0, leading: 0, bottom: 0, trailing: 0)
+            
+            return section
+        }
+    }
+    
+    private func configureDataSource() {
+        let datasource = RandomCatDataSource(collectionView: self.randomCollectionView, cellProvider: {(collectionView, indexPath, item) -> UICollectionViewCell in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RandomCatCollectionViewCell.identifier, for: indexPath) as? RandomCatCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            guard let item = item as? RandomCatCollectionViewCell else { return cell }
+            
+            return cell
+        })
+        
+        self.dataSource = datasource
+        self.randomCollectionView.dataSource = datasource
+    }
+}
 
+extension RandomCatViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath)
+    }
 }
 
