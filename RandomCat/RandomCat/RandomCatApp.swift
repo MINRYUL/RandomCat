df34ad805ee9333e0814725565b420699e1d5cc1
MinChang <rud0806@gmail.com>
2022-01-12 16:47:40 +0900

    [Feat] init Project

---
+++ RandomCat/RandomCat/RandomCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomCatApp.swift
+//  RandomCat
+//
+//  Created by 김민창 on 2022/01/12.
+//
+
+import SwiftUI
+
+@main
+struct RandomCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
