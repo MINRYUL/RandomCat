44c9ffffcb1f8a6d1bac7abaf3955ecd5a2254c2
MinChang <rud0806@gmail.com>
2022-01-12 17:45:34 +0900

    [Feat] Configure RandomCat CollectionView

--- RandomCat/RandomCat/Presentation/Controller/RandomCatViewController.swift
+++ RandomCat/RandomCat/Presentation/Controller/RandomCatViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class RandomCatViewController: UIViewController {
-
+    lazy private var randomCollectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: 0, height: 0), collectionViewLayout: flowLayout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
+    private func configureView() {
+        self.view.addSubview(self.randomCollectionView)
+        
+        NSLayoutConstraint.activate([
+            self.randomCollectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.randomCollectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.randomCollectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            self.randomCollectionView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
 
 }
 
